248ef001bcc24491099d6e0e34ea0000dbe0e158
abdollahebadi <abdollah.ebadi@gmail.com>
2018-12-12 12:09:07 +1100

    Changes: - AVAudioPlayer functionality added for playing	sound tracks. - Error checking and message printing added

--- Xylophone/ VIewController.swift
+++ Xylophone/ VIewController.swift
@@ -7,10 +7,15 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController{
     
-
+    var audioPlayer : AVAudioPlayer?
+    
+    
+    let noteList : [String] = ["note1" , "note2" , "note3" ,"note4" , "note5" , "note6" , "note7"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
@@ -19,7 +24,25 @@ class ViewController: UIViewController{
 
     @IBAction func notePressed(_ sender: UIButton) {
         
-        
+        if let resourcePath = Bundle.main.path(forResource: noteList[sender.tag - 1], ofType: "wav") {
+            let noteSound : URL?
+            noteSound = URL(fileURLWithPath: resourcePath)
+            
+            do {
+                audioPlayer =  try AVAudioPlayer.init(contentsOf: noteSound! )
+            }
+            catch {
+                print("Error in obtaining AVAudioPlayer")
+            }
+            
+            if let audioP = audioPlayer {
+                audioP.prepareToPlay()
+                audioP.play()
+            }
+        }
+        else {
+            print("Error - Unable to find sound track in resource folder")
+        }
         
     }
     
